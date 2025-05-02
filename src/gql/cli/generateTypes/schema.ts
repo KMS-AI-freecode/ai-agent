beb0f3cb5306c2996ebbf4cf5045f66b7df081fc
Nikolay Lanets <n.lanets@freecode.academy>
2025-05-02 06:53:15 +0300

    Adde codegen

---
+++ src/gql/cli/generateTypes/schema.ts
@@ -0,0 +1,18 @@
+import * as codegen from '@graphql-codegen/cli'
+import path from 'path'
+import { OUTPUT_PATH } from './constants'
+
+/** Функция генерирующая schema.json */
+export const generateSchema = async () => {
+  await codegen.generate(
+    {
+      schema: path.resolve(process.cwd(), 'server/graphql/schema.ts'),
+      generates: {
+        [path.resolve(OUTPUT_PATH, 'schema.json')]: {
+          plugins: [{ introspection: {} }],
+        },
+      },
+    },
+    true,
+  )
+}
